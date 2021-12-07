23f0bc806e578a2c08c1d695af3fab98b33f58ae
miku <lou0xue@126.com>
2021-12-07 09:34:19 +0800

    2021-12-7

--- dist/client/8a36e92.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{640:function(n,t,e){"use strict";e.r(t);var c={asyncData:function(n){(0,n.redirect)("/")},data:function(){return{}}},r=e(32),component=Object(r.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
